120b06bb58ec78e47eaceb35f2e7c67ed3ee1501
simrankaur7575 <simransaini7575@gmail.com>
2020-04-21 18:19:09 +0530

    first activity added

---
+++ app/src/main/java/com/example/travallocations/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.travallocations
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_place,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_place_option) {
+
+//                val intent = Intent(applicationContext,
+//                    MapsActivity::class.java)
+//                intent.putExtra("info","new")
+//                startActivity(intent)
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
